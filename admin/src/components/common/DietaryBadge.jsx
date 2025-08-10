f0b78404dec08f97eef6511a92dbcf617c9db92d
Sahil Purav <sahil.purav@live.com>
2025-08-10 21:43:29 +0530

    fix: reduced dietary badge size

--- admin/src/components/common/DietaryBadge.jsx
+++ admin/src/components/common/DietaryBadge.jsx
@@ -1,12 +1,12 @@
 export const DietaryBadge = ({ isVeg }) => {
   return (
-    <div className={`flex bg-white items-center justify-center w-5 h-5 border-2 ${
+    <div className={`flex bg-white items-center justify-center w-4 h-4 border-2 ${
       isVeg ? 'border-green-600' : 'border-red-500'
     }`}>
       {isVeg ? (
-        <div className="w-3.5 h-3.5 rounded-full bg-green-600" />
+        <div className="w-2.5 h-2.5 rounded-full bg-green-600" />
       ) : (
-        <div className="w-0 h-0 border-l-[7px] border-r-[7px] border-b-[9px] border-l-transparent border-r-transparent border-b-red-500 rounded-sm" />
+        <div className="w-0 h-0 border-l-[5px] border-r-[5px] border-b-[7px] border-l-transparent border-r-transparent border-b-red-500 rounded-sm" />
       )}
     </div>
   );
