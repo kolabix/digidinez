d04aec5b0c9f91d225c3e94fdea188d020ef185b
Sahil Purav <sahil.purav@live.com>
2025-08-11 15:38:31 +0530

    fix: Changed the base to /

--- public-menu/vite.config.js
+++ public-menu/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE || '/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss()
