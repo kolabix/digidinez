23afd68f72261829f4fec5d87a8afbc676042557
Sahil Purav <sahil.purav@live.com>
2025-08-11 15:54:14 +0530

    added back the vite base

--- public-menu/vite.config.js
+++ public-menu/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.VITE_BASE || '/',
   plugins: [
     react(),
     tailwindcss()
